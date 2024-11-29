d1af0f8176c5f9871c9fa2bebc4693dbf51f697a
Niels Weistra <n.weistra@itlusions.nl>
2024-11-29 21:31:18 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -25,10 +25,6 @@ app.mount("/v1_mock", app_v1_mock)
 async def health_check():
     return JSONResponse(content={"status": "healthy"}, status_code=200)
 
-@app.get("/")
-async def root():
-    return RedirectResponse(url="/v1")
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
